343016cae097c2d5524eb1f4b319e9c45c06419b
jjcsf <jjcsf@users.noreply.github.com>
2015-11-07 23:32:04 -0800

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,7 @@
+dt<-read.delim("household_power_consumption.txt", sep=";")
+dt<-dt[dt$Date %in% c('1/2/2007','2/2/2007'),]
+
+png(file="plot2.png",width = 480, height = 480)
+
+with(dt,plot(strptime(paste(dt$Date, dt$Time), "%d/%m/%Y %H:%M:%S"),as.numeric(as.character(dt$Global_active_power)),type="l",ylab="Global Active Power (kilowatts)",xlab=""))
+dev.off()
